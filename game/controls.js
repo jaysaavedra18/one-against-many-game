f190c54959d90514d021add38d0540c7bf220b23
jaysaavedra18 <jasonsaavedra@yahoo.com>
2025-01-18 13:48:27 -1000

    added arrow keys to the event listeners' switch statements in controls.js

--- game/controls.js
+++ game/controls.js
@@ -17,21 +17,33 @@ export class Controls {
 
     handleKeyDown(e) {
         switch (e.code) {
+            // Movement controls
             case "KeyW": this.keys.w.pressed = true; break;
             case "KeyA": this.keys.a.pressed = true; break;
             case "KeyS": this.keys.s.pressed = true; break;
             case "KeyD": this.keys.d.pressed = true; break;
             case "Space": this.keys.space.pressed = true; break;
+            // Camera controls
+            case "ArrowUp": this.keys.arrowUp.pressed = true; break;
+            case "ArrowDown": this.keys.arrowDown.pressed = true; break;
+            case "ArrowLeft": this.keys.arrowLeft.pressed = true; break;
+            case "ArrowRight": this.keys.arrowRight.pressed = true; break;
         }
     }
 
     handleKeyUp(e) {
         switch (e.code) {
+            // Movement controls
             case "KeyW": this.keys.w.pressed = false; break;
             case "KeyA": this.keys.a.pressed = false; break;
             case "KeyS": this.keys.s.pressed = false; break;
             case "KeyD": this.keys.d.pressed = false; break;
             case "Space": this.keys.space.pressed = false; break;
+            // Camera controls
+            case "ArrowUp": this.keys.arrowUp.pressed = false; break;
+            case "ArrowDown": this.keys.arrowDown.pressed = false; break;
+            case "ArrowLeft": this.keys.arrowLeft.pressed = false; break;
+            case "ArrowRight": this.keys.arrowRight.pressed = false; break;
         }
     }
 }
